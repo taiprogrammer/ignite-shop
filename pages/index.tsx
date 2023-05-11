496a97619bdf79266650721d797ee5719f7587b2
taiprogrammer <taiza.marques@sptech.school>
2023-05-11 15:44:31 -0300

    init stitches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Button>Teste</Button>
     </>
   );
 }
