188c25ceca4adfb043a3b149c14ddf40f1104ae1
taiprogrammer <taiza.marques@sptech.school>
2023-05-31 18:13:50 -0300

    formatting price

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({ products }: HomeProps) {
               <Image src={product.imageUrl} width={520} height={480} alt="" />
               <footer>
                 <strong>{product.name}</strong>
-                <span>R$ {product.price / 100}</span>
+                <span>{product.price}</span>
               </footer>
             </Product>
           );
@@ -54,7 +54,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount,
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(price.unit_amount / 100),
     };
   });
 
