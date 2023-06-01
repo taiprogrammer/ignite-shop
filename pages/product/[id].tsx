e59de397fe8c42f133d5323cbbbe15dbb72d310d
taiprogrammer <taiza.marques@sptech.school>
2023-06-01 20:14:39 -0300

    product page structure

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../product";
+import Image from "next/image";
 
 export default function Products() {
   const router = useRouter();
 
   return (
-    <h1>
-      Products <span>{JSON.stringify(router.query)}</span>
-    </h1>
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 60,00</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ratione
+          error officia voluptas tempora? Inventore perspiciatis officia
+          eveniet, deserunt repudiandae sunt ipsum ad minima necessitatibus
+          distinctio aliquam, quam aspernatur vitae.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
